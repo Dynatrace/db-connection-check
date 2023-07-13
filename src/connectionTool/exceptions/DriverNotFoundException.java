f71e30451ed39e9fa8231c6701f0ea1a4726866f
Kasper.Serzysko <kasper.serzysko@dynatrace.com>
2023-07-13 16:45:48 +0200

    minor fixes

---
+++ src/connectionTool/exceptions/DriverNotFoundException.java
@@ -0,0 +1,8 @@
+package connectionTool.exceptions;
+
+public class DriverNotFoundException extends Exception{
+
+    public DriverNotFoundException(String message) {
+        super(message);
+    }
+}
