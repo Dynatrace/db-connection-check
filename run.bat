3ce8decb31121f64694a7f9895a53686e9bc1aca
Kasper.Serzysko <kasper.serzysko@dynatrace.com>
2023-08-09 17:39:42 +0200

    fixed run

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 @echo off
-java -cp lib/*;. connectionTool.Main %*
+java -cp lib\jcommander-1.82.jar;lib\junit-jupiter-api-5.9.3.jar;lib\junit-jupiter-engine-5.9.3.jar;lib\junit-platform-commons-1.9.3.jar;lib\junit-platform-engine-1.9.3.jar;. connectionTool.Main %*
